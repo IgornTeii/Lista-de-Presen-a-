2ffb8762144757a98442259cd6b7d41296a59430
Igor <igornteixeira48@gmail.com>
2024-04-22 23:00:50 -0300

    feature: test

--- src/br/com/fiap/listapresenca/Pessoa.java
+++ src/br/com/fiap/listapresenca/Pessoa.java
@@ -1,28 +1,26 @@
 package br.com.fiap.listapresenca;
 
 public abstract class Pessoa {
-  //Campos protegidos: são acessíveis dentro da própria classe e por classes que a estendem.
   protected String nome;
   protected int idade;
   protected String sexo;
 
   public Pessoa(String nome, int idade, String sexo) {
-    this.nome = nome; // 'this' é usado para referenciar o campo da instância atual.
+    this.nome = nome;
     this.idade = idade;
     this.sexo = sexo;
   }
-  //Método abstrato cadastrar: será implementado pelas subclasses.
+
   public abstract void cadastrar();
 
   public abstract void registrarPresenca();
 
-  //Getters e setters para nome, idade e sexo. Estes permitem o acesso seguro aos campos.
   public String getNome() {
     return nome;
   }
 
   public void setNome(String nome) {
-    this.nome = nome; // Atualiza o nome da instância atual.
+    this.nome = nome;
   }
 
   public int getIdade() {
@@ -30,11 +28,11 @@ public abstract class Pessoa {
   }
 
   public void setIdade(int idade) {
-    this.idade = idade; // Atualiza a idade da instância atual.
+    this.idade = idade;
   }
 
   public void setSexo(String sexo) {
-    this.sexo = sexo; // Atualiza o sexo da instância atual.
+    this.sexo = sexo;
   }
 
   public String getSexo() {
--- src/br/com/fiap/teste/Teste.java
+++ src/br/com/fiap/teste/Teste.java
@@ -1,7 +1,17 @@
 package br.com.fiap.teste;
 
+import br.com.fiap.listapresenca.Aluno;
+import br.com.fiap.listapresenca.Professor;
+
 public class Teste {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Aluno aluno = new Aluno("Nome do Aluno", 20, "Masculino", "20202020", "Engenharia de Software");
+        Professor professor = new Professor("Nome do Professor", 40, "Masculino", "20202021", "Matemática");
+
+        aluno.cadastrar();
+        professor.cadastrar();
+
+        aluno.registrarPresenca();
+        professor.registrarPresenca();
     }
 }
